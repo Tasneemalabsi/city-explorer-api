6debb2431cbfbc49f7f2149aadea9607b7182f97
Tasneemalabsi <tasneem.alabsi@gmail.com>
2021-08-17 02:41:47 +0300

    backend finish

--- server.js
+++ server.js
@@ -3,17 +3,39 @@
 const express = require('express');
 require('dotenv').config();
 const server = express();
-// const cors = require('cors');
+const cors = require('cors');
 const weatherData = require('./data/weather.json')
 
 
 
 const PORT = process.env.PORT || 3001 ;
-// server.use(cors());
+server.use(cors());
 
 server.get('/weather', (request,response)=>{
-    let lat = req.query.lat;
-    let lon= req.query.lon;
+    let lat = request.query.lat;
+    let lon= request.query.lon;
+    let searchquery=request.query.searchquery;
+    let arr = []
+
+    
+    try{
+    let renderedData =weatherData.find(city=>{
+        if( city.city_name.toLowerCase()===searchquery) {
+            return city;
+        }
+    });
+    
+   renderedData.data.map(city=>{
+       let cityData=new Forecast (city.datetime, `Low of ${city.low_temp}, high of ${city.max_temp} with ${city.weather.description}`);
+       arr.push(cityData);
+   }
+)
+response.send(arr)}
+catch {
+    response.send('{"error"  :  "Something went wrong."}');
+}
+
+
 
 })
 
@@ -22,6 +44,14 @@ server.listen(PORT,()=>{
     console.log(`Listning on PORT ${PORT}`)
 })
 
+class Forecast {
+ constructor(date, desc){
+        this.date = date;
+        this.desc=desc;
+         
+    }
+
+}
 
 
 
