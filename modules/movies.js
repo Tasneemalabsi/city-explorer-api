eb18eda605a5b63326c9af998c19d8eb076804c4
Tasneemalabsi <tasneem.alabsi@gmail.com>
2021-08-18 19:04:39 +0300

    finish refactoring

---
+++ modules/movies.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+module.exports = handleMoviesData;
+
+async function handleMoviesData(req,res){
+    let query= req.query.query;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_KEY}&query=${query}`
+    let arr = [];
+    try{
+        let movieData= await axios.get(url);
+        
+            console.log('the search city is', query);
+       movieData.data.results.map(city=>{
+           
+        
+           let cityMovieData=new movieDataConst (city.title, city.overview, city.vote_average,city.vote_count,`https://image.tmdb.org/t/p/w500${city.poster_path}`,city.popularity,city.release_date);
+           
+           arr.push(cityMovieData);   }
+    )
+    res.send(arr)
+        }
+        
+    catch(error) {
+        console.log('THE ERROR IS :',error);
+        res.send(error);
+    }
+    
+    
+    }
+    
+    
+    class movieDataConst {
+        constructor(title,overview,average_votes,total_votes,image_url,popularity,released_on){
+            this.title=title;
+            this.overview=overview;
+            this.average_votes=average_votes;
+            this.total_votes=total_votes;
+            this.image_url=image_url;
+            this.popularity=popularity;
+            this.released_on=released_on
+        }
+    }
